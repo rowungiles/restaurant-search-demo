461518181ee69718db3bc4f1abdc1de23c112c0c
Rowun Giles <1868220+rowungiles@users.noreply.github.com>
2018-03-26 09:52:51 +0100

    Single view project setup

---
+++ RestaurantSearchDemo/RestaurantSearchDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  RestaurantSearchDemo
+//
+//  Created by Rowun Giles on 26/03/2018.
+//  Copyright © 2018 Rowun Giles. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
